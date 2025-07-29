b5444a1be150b523973225ad54eb4dff8bb03a52
David200201 <Jeremydavced@gmail.com>
2025-07-28 21:05:13 -0500

    Funcionalidad para el botón registrarse - Actualización2#3

--- biblioteca/backend/src/routes/ejemplo.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ejemploController = require('../controllers/ejemploController');
-
-// Ruta para recibir datos desde el frontend
-router.post('/endpoint', ejemploController.recibirDatos);
-
-module.exports = router;
